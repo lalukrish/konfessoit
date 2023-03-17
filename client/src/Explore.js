88f14734379447c7952c9c4bfa7fd49d598551cf
lalukrish <lalukrishnavallil01@gmail.com>
2023-03-17 21:13:18 +0530

    things

--- client/src/Explore.js
+++ client/src/Explore.js
@@ -1,8 +1,8 @@
-import React ,{useState}from "react";
+import React, { useState } from "react";
 import HeaderM from "./Map/HeaderM/HeaderM";
 import List from "./Map/List/List";
 import { CssBaseline, Grid } from "@mui/material";
-import Maps from "./Map/Maps/Maps";
+// import Maps from "./Map/Maps/Maps";
 import ListPlace from "./Map/List/List";
 const Explore = () => {
   const [selectPosition, setSelectPosition] = useState(null);
@@ -14,13 +14,17 @@ const Explore = () => {
         flexDirection: "row",
         width: "100vw",
         height: "100vh",
-      }} className="map"
+      }}
+      className="map"
     >
       <div style={{ width: "50vw", height: "100%" }}>
-        <Maps selectPosition={selectPosition} />
+        {/* <Maps selectPosition={selectPosition} /> */}
       </div>
       <div style={{ width: "50vw" }}>
-        <ListPlace selectPosition={selectPosition} setSelectPosition={setSelectPosition}/>
+        <ListPlace
+          selectPosition={selectPosition}
+          setSelectPosition={setSelectPosition}
+        />
       </div>
     </div>
   );
